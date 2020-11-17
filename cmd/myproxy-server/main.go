c4ea018dc704fe3b82d2d9213896c19e538c4d28
elo5 <elo5@qq.com>
2020-11-17 12:28:45 +0800

    server done

--- cmd/myproxy-server/main.go
+++ cmd/myproxy-server/main.go
@@ -2,29 +2,86 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net"
+	"os"
+	"strconv"
 
 	"github.com/myproxy"
+	"github.com/myproxy/cmd"
+	"github.com/myproxy/server"
+	"github.com/phayes/freeport"
 )
 
 var version = "master"
 
 func main() {
-	// log.SetFlags(log.Lshortfile)
+	log.SetFlags(log.Lshortfile)
 
-	// // 优先从环境变量中获取监听端口
-	// port, err := strconv.Atoi(os.Getenv("MY_PROXY_SERVER_PORT"))
-	// // 服务端监听端口随机生成
-	// if err != nil {
-	// 	port, err = freeport.GetFreePort()
+	// 优先从环境变量中获取监听端口
+	port, err := strconv.Atoi(os.Getenv("MY_PROXY_SERVER_PORT"))
+	// 服务端监听端口随机生成
+	if err != nil {
+		port, err = freeport.GetFreePort()
+	}
+
+	if err != nil {
+		port = 7448 // 随机端口失败就采用 7448
+	}
+	// 默认配置
+	config := &cmd.Config{
+		ListenAddr: fmt.Sprintf(":%d", port),
+		// 密码随机生成
+		Password: myproxy.RandomPassword(),
+	}
+
+	config.ReadConfig()
+	config.SaveConfig()
+
+	myserver, err := server.NewLsServer(config.Password, config.ListenAddr)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Fatalln(myserver.Listen(func(listenAddr *net.TCPAddr) {
+		log.Println(fmt.Sprintf(`
+myproxy-server:%s  started successfully, configuration as follow:
+Listen to address:
+%s
+password:
+%s`, version, listenAddr, config.Password))
+	}))
+
+	// --------------  test code  --------------
+
+	// pwd := myproxy.RandomPassword()
+
+	// fmt.Printf("随机密码: %s\n", pwd)
+
+	// a := rand.Int()
+	// b := rand.Intn(100)
+
+	// fmt.Println(a)
+	// fmt.Println(b)
+
+	// rand.Seed(time.Now().Unix())
+
+	// for i := 0; i < 10; i++ {
+	// 	fmt.Println(rand.Intn(100))
 	// }
-
-	// if err != nil {
-	// 	port = 7448
-	// }
-	// config := &cmd.Config{}
-
-	pwd := myproxy.RandomPassword()
-
-	fmt.Printf("随机密码: %s\n", pwd)
 
 }
+
+// func Test1() {
+// 	rand.Seed(time.Now().Unix())
+// 	for i := 0; i < 10; i++ {
+// 		//设置随机数种子
+// 		bytes := make([]byte, 5)
+// 		for i := 0; i < 5; i++ {
+// 			b := rand.Intn(26) + 65
+// 			fmt.Println(b)
+// 			bytes[i] = byte(b)
+// 		}
+// 		fmt.Println(string(bytes))
+// 	}
+// }
--- password.go
+++ password.go
@@ -5,10 +5,8 @@ import (
 	"errors"
 	"fmt"
 	"math/rand"
-	"reflect"
 	"strings"
 	"time"
-	"unsafe"
 )
 
 const passwordLength = 256
@@ -26,11 +24,11 @@ func (password *Password) String() string {
 	return base64.StdEncoding.EncodeToString(password[:])
 }
 
-func BytesToString(b []byte) string {
-	bh := (*reflect.SliceHeader)(unsafe.Pointer(&b))
-	sh := reflect.StringHeader{bh.Data, bh.Len}
-	return *(*string)(unsafe.Pointer(&sh))
-}
+// func BytesToString(b []byte) string {
+// 	bh := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+// 	sh := reflect.StringHeader{bh.Data, bh.Len}
+// 	return *(*string)(unsafe.Pointer(&sh))
+// }
 
 // ParsePassword 解析采用base64编码的字符串获取密码
 func ParsePassword(passwordString string) (*Password, error) {
@@ -51,37 +49,16 @@ func RandomPassword() string {
 	intArr := rand.Perm(passwordLength)
 	password := &Password{}
 
+	// fmt.Println(intArr)
+	// fmt.Println("----------------------------------- ")
+
 	for i, v := range intArr {
 		password[i] = byte(v)
 		fmt.Println("for  loop :", i, " -- ", v)
-
 		if i == v {
 			// 确保不会出现如何一个byte位出现重复
 			return RandomPassword()
 		}
 	}
-
-	/***************************************/
-
-	// 方法一：
-	// int *p=(int *)0x123456;
-	// int result=*p;
-	// 首先要将地址赋值给指针变量，通过指针读取内容。
-	// 方法二：
-	// int result=*(int *)0x123456;
-	// 直接读取地址内容
-
-	// str1 := BytesToString(*(Password*)password)
-	// fmt.Println("String:", str1)
-
-	// /****************************************/
-
-	// str2 := string(byteArray1[:])
-	// fmt.Println("String:", str2)
-
-	// /****************************************/
-	// str3 := bytes.NewBuffer(byteArray1).String()
-	// fmt.Println("String:", str3)
-	fmt.Println("String:", password)
 	return password.String()
 }
